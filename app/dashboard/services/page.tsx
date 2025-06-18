c3b2e0c5e6201254bd6ef49290430b8c56dc082b
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-06-18 17:14:17 +0600

    bug fiexed

--- app/dashboard/services/page.tsx
+++ app/dashboard/services/page.tsx
@@ -32,7 +32,7 @@ export default function ServicesPage() {
 
   const fetchServices = () => {
     setIsLoading(true)
-    getServices(searchTerm)
+    getServices()
       .then((data) => {
         if (data) setServices(data)
       })
