ba2c6cb28026d849cab4e79b21dd701df1c6d112
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-07-02 19:26:37 +0600

    dashboard page bug fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -61,7 +61,9 @@ export default function DashboardPage() {
       }
     };
 
-    fetchDashboardSummary();
+    if (currentBranchId) {
+      fetchDashboardSummary();
+    }
   }, [currentBranchId, dateRange]);
 
   // Sample data based on your structure
